5209190aa6cc91bec1092b6ba5cab040e3fce326
Joshua Ellis <joshua@wideeyecreative.com>
2024-04-06 21:24:09 -0400

    bump deps

--- countries/index.ts
+++ countries/index.ts
@@ -3,30 +3,12 @@ import type {I18nLocale, Localizations} from '../app/lib/type';
 
 export const countries: Localizations = {
   default: {
-    country: 'CA',
-    currency: 'CAD',
-    isoCode: 'en-ca',
-    label: 'Canada (CAD $)',
+    country: 'US',
+    currency: 'USD',
+    isoCode: 'en-US',
+    label: 'United States ($ USD)',
     language: 'EN',
     languageLabel: 'English',
-    salesChannel: 'hydrogen',
-  },
-  '/ca-fr': {
-    country: 'CA',
-    currency: 'CAD',
-    isoCode: 'fr-ca',
-    label: 'Canada - French (CAD $)',
-    language: 'FR',
-    languageLabel: 'French',
-    salesChannel: 'hydrogen',
-  },
-  '/fr': {
-    country: 'FR',
-    currency: 'EUR',
-    isoCode: 'fr-fr',
-    label: 'France (EUR €)',
-    language: 'FR',
-    languageLabel: 'French',
     salesChannel: 'hydrogen',
   },
 };
