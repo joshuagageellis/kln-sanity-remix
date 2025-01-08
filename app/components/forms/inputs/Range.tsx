b18c32f8308b0adab648abc80b99de32e318fb45
Joshua Ellis <joshua@wideeyecreative.com>
2025-01-08 13:34:08 -0500

    contact form polishing

--- app/components/forms/inputs/Range.tsx
+++ app/components/forms/inputs/Range.tsx
@@ -1,94 +1,113 @@
-import type { FieldHookConfig } from "formik";
+import type {FieldHookConfig} from 'formik';
 
-import { useField } from "formik";
-import { useCallback, useRef } from "react";
+import {useField} from 'formik';
+import {useCallback, useRef} from 'react';
 
-import { cn } from "~/lib/utils";
+import {cn} from '~/lib/utils';
 
-import { ErrorMessageWrapper, labelClass } from "../ContactForm";
+import {ErrorMessageWrapper, labelClass} from '../ContactForm';
 
 const formatAsCurrency = (value: number) => {
-	return value.toLocaleString('en-US', {
-		currency: 'USD',
-		maximumFractionDigits: 0,
-		minimumFractionDigits: 0,
-  	style: 'currency'
-	});
-}
+  return value.toLocaleString('en-US', {
+    currency: 'USD',
+    maximumFractionDigits: 0,
+    minimumFractionDigits: 0,
+    style: 'currency',
+  });
+};
 
-export function SliderField({currency, label, max, min, postFix = '', unlimited, ...props}: {
-	currency?: boolean;
-	label: string;
-	max: number;
-	min: number;
-	postFix?: string;
-	unlimited?: boolean; // Adds a '+' to the max value
-} & FieldHookConfig<any> & JSX.IntrinsicElements['input']) {
-	const [field,, helpers] = useField(props);
-	const fieldValue = field.value as [number, number];
+export function SliderField({
+  currency,
+  label,
+  max,
+  min,
+  postFix = '',
+  unlimited,
+  ...props
+}: {
+  currency?: boolean;
+  label: string;
+  max: number;
+  min: number;
+  postFix?: string;
+  unlimited?: boolean; // Adds a '+' to the max value
+} & FieldHookConfig<any> &
+  JSX.IntrinsicElements['input']) {
+  const [field, , helpers] = useField(props);
+  const fieldValue = field.value as [number, number];
   const range = useRef<HTMLDivElement>(null);
-	const step = Number(props.step || 1);
+  const step = Number(props.step || 1);
 
   const getPercent = useCallback(
     (value: number) => Math.round(((value - min) / (max - min)) * 100),
-    [min, max]
+    [min, max],
   );
 
-	return (
-		<>
-			<div className='grow basis-1/2'>
-				<label className={cn(labelClass)} htmlFor={field?.name}>{label}</label>
-				<div className="relative h-16 mt-6">
-					<input className={cn([
-						'thumb',
-					])} max={max}
-					min={min}
-					onChange={(event) => {
-						const value = Math.min(Number(event.target.value), fieldValue[1] - step);
-							helpers.setValue([value, fieldValue[1]]);
-						}}
-						step={step}
-						type="range"
-						value={fieldValue[0]}
-					/>
-					<input className={cn([
-						'thumb',
-					])} max={max}
-					min={min}
-					onChange={(event) => {
-						const value = Math.max(Number(event.target.value), fieldValue[0] + step);
-						helpers.setValue([fieldValue[0], value]);
-					}}
-					step={step}
-					type="range"
-					value={fieldValue[1]}
-					/>
-					<div className="[&>*]:absolute">
-						{/* track */}
-						<div className="rounded-2 bg-panther z-10 w-full h-2 top-0" />
-						{/* range */}
-						<div
-							className="rounded-2 bg-citrus z-20 h-2 top-0"
-							ref={range}
-							style={{
-								left: `${getPercent(fieldValue[0])}%`,
-								width: `${getPercent(fieldValue[1]) - getPercent(fieldValue[0])}%`
-							}}
-						/>
-						{/* left */}
-						<div className="text-charcoal h6 mt-8 left-0">
-							{currency ? formatAsCurrency(fieldValue[0]) : fieldValue[0]}{` ${postFix}`}
-						</div>
-						{/* right */}
-						<div className="text-charcoal h6 mt-8 right-0">
-							{(unlimited && fieldValue[1] === max) && '+'}{currency ? formatAsCurrency(fieldValue[1]) : fieldValue[1]}{` ${postFix}`}
-						</div>
-					</div>
-				</div>
-				<input className="hidden" tabIndex={-1} {...field} {...props} />
-				<ErrorMessageWrapper name={field.name} />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className={cn(['grow basis-1/2'])}>
+        <label className={cn(labelClass)} htmlFor={field?.name}>
+          {label}
+        </label>
+        <div className="relative mt-6 h-16">
+          <input
+            className={cn(['thumb'])}
+            max={max}
+            min={min}
+            onChange={(event) => {
+              const value = Math.min(
+                Number(event.target.value),
+                fieldValue[1] - step,
+              );
+              helpers.setValue([value, fieldValue[1]]);
+            }}
+            step={step}
+            type="range"
+            value={fieldValue[0]}
+          />
+          <input
+            className={cn(['thumb'])}
+            max={max}
+            min={min}
+            onChange={(event) => {
+              const value = Math.max(
+                Number(event.target.value),
+                fieldValue[0] + step,
+              );
+              helpers.setValue([fieldValue[0], value]);
+            }}
+            step={step}
+            type="range"
+            value={fieldValue[1]}
+          />
+          <div className="[&>*]:absolute">
+            {/* track */}
+            <div className="rounded-2 top-0 z-10 h-2 w-full bg-panther" />
+            {/* range */}
+            <div
+              className="rounded-2 top-0 z-20 h-2 bg-citrus"
+              ref={range}
+              style={{
+                left: `${getPercent(fieldValue[0])}%`,
+                width: `${getPercent(fieldValue[1]) - getPercent(fieldValue[0])}%`,
+              }}
+            />
+            {/* left */}
+            <div className="h6 left-0 mt-8 text-charcoal">
+              {currency ? formatAsCurrency(fieldValue[0]) : fieldValue[0]}
+              {` ${postFix}`}
+            </div>
+            {/* right */}
+            <div className="h6 right-0 mt-8 text-charcoal">
+              {unlimited && fieldValue[1] === max && '+'}
+              {currency ? formatAsCurrency(fieldValue[1]) : fieldValue[1]}
+              {` ${postFix}`}
+            </div>
+          </div>
+        </div>
+        <input className="hidden" tabIndex={-1} {...field} {...props} />
+        <ErrorMessageWrapper name={field.name} />
+      </div>
+    </>
+  );
 }
-
--- app/routes/contact.send.tsx
+++ app/routes/contact.send.tsx
@@ -1,197 +1,237 @@
 import type {ActionFunctionArgs} from '@shopify/remix-oxygen';
-import type { InferType } from 'yup';
+import type {InferType} from 'yup';
 
 import {json} from '@shopify/remix-oxygen';
-import {ValidationError, number, object, string, tuple } from 'yup';
+import {renderToString} from 'react-dom/server';
+import {ValidationError, number, object, string, tuple} from 'yup';
 
 export const serviceTypes = {
-	cnc: 'CNC',
-	design_fabrication: 'Design & Fabrication',
-	get_our_products: 'Get our Products',
-	other: 'Other (I don’t know)',
-}
+  cnc: 'CNC',
+  design_fabrication: 'Design & Fabrication',
+  get_our_products: 'Get our Products',
+  other: 'Other (I don’t know)',
+};
 
-export const serviceTypesValues = Object.values(serviceTypes)
+export const serviceTypesValues = Object.values(serviceTypes);
 
 export const deliveryOptions = {
-	inperson: 'Delivery/Installation',
-	pickup: 'Pickup',
-}
+  inperson: 'Delivery/Installation',
+  pickup: 'Pickup',
+};
 
-export const deliveryOptionsValues = Object.values(deliveryOptions)
+export const deliveryOptionsValues = Object.values(deliveryOptions);
 
 export const contactForminitialValues = {
-	addressLine1: '',
-	addressLine2: '',
-	budget: [15000, 30000],
-	city: '',
-	country: 'United States',
-	delivery: '',
-	email: '',
-	firstName: '',
-	lastName: '',
-	links: '',
-	phoneNumber: '',
-	projectBrief: '',
-	serviceType: '',
-	state: '',
-	timeline: [1, 3],
-	zip: '',
-}
+  addressLine1: '',
+  addressLine2: '',
+  budget: [5000, 15000],
+  city: '',
+  country: 'United States',
+  delivery: '',
+  email: '',
+  firstName: '',
+  lastName: '',
+  links: '',
+  phoneNumber: '',
+  projectBrief: '',
+  serviceType: '',
+  state: '',
+  timeline: [3, 6],
+  zip: '',
+};
 
-const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
-const submissionEmailFormat = (data: ContactForm) => {
-	return `
-		First Name: ${data.firstName}\n
-		Last Name: ${data.lastName}\n
-		Email: ${data.email}\n
-		Phone: ${data.phoneNumber}\n
-		Timeline: ${data.timeline}\n
-		Budget: ${data.budget}\n
-		Service Type: ${data.serviceType}\n
-		Project Brief: ${data.projectBrief}\n
-	`
-}
+const splitString = (str: string, id: string) => str.split('\n').map((str, index) => <p key={`${index}-${id}`}>{str}</p>);
 
-export const contactFormSchema = object({
-	addressLine1: string().when('delivery', {
-		is: deliveryOptions.inperson,
-		then: (schema) => schema.required('Primary address is required')
-	}),
-	addressLine2: string(),
-	budget: tuple([
-		number(),
-		number()
-	]),
-	city: string().when('delivery', {
-		is: deliveryOptions.inperson,
-		then: (schema) => schema.required('City is required')
-	}),
-	country: string().when('delivery', {
-		is: deliveryOptions.inperson,
-		then: (schema) => schema.required('Country is required')
-	}),
-	delivery: string().oneOf(deliveryOptionsValues).required('Delivery option is required'),
-	email: string().email().required(
-		'Email is required'
-	),
-	firstName: string().required(
-		'First name is required'
-	),
-	lastName: string().required(
-		'Last name is required'
-	),
-	links: string().when('serviceType', {
-		is: serviceTypes.cnc,
-		then: (schema) => schema.required('Please provide links to your files')
-	}).when('serviceType', {
-		is: serviceTypes.design_fabrication,
-		then: (schema) => schema.required('Please provide links to your files')
-	}),
-	phoneNumber: string().matches(phoneRegExp, 'Phone number is not valid'),
-	projectBrief: string().required(
-		'Project brief is required'
-	),
-	serviceType: string().oneOf(serviceTypesValues).required(
-		'Service type is required'
-	),
-	state: string().when('delivery', {
-		is: deliveryOptions.inperson,
-		then: (schema) => schema.required('State is required')
-	}),
-	timeline: tuple([
-		number(),
-		number()
-	]),
-	zip: string().when('delivery', {
-		is: deliveryOptions.inperson,
-		then: (schema) => schema.required('Zip is required')
-	})
-});
+const submissionEmailFormat = (data: ContactForm) =>
+  renderToString(
+    <div>
+      <div>
+        <h1>New Website Inquiry, RE: {data.serviceType}</h1>
+				<div>
+					<h2>Contact</h2>
+					<p>Name: {data.firstName} {data.lastName}</p>
+					<p>Email: {data.email}</p>
+					{data.phoneNumber && <p>Phone: {data.phoneNumber}</p>}
+				</div>
+				{data.serviceType === serviceTypes.design_fabrication || data.serviceType === serviceTypes.cnc ? (
+					<>
+						<div>
+							<h2>Project Details</h2>
+							<p>Service Type: {data.serviceType}</p>
+							<p>Project Brief: {splitString(data.projectBrief, 'brief')}</p>
+							{data.links && <p>Links: {splitString(data.links, 'links')}</p>}
+							{data.timeline && data.timeline.length > 2 && <p>Timeline: {data.timeline[0]} - {data.timeline[1]} months</p>}
+							{data.budget && data.budget.length > 2 && <p>Budget: ${data.budget[0]} - {data.budget[1]}</p>}
+						</div>
+						<div>
+							<h2>Delivery & Pickup Options</h2>
+							<p>Delivery: {data.delivery}</p>
+							{data.delivery === deliveryOptions.inperson && (
+								<>
+									<p>Address: {data.addressLine1}</p>
+									<p>Address 2: {data.addressLine2}</p>
+									<p>City: {data.city}</p>
+									<p>State: {data.state}</p>
+									<p>Zip: {data.zip}</p>
+									<p>Country: {data.country}</p>
+								</>
+							)}
+						</div>
+					</>
+				) : (
+					<div>
+						<h2>Service Type</h2>
+						<p>Service Type: {data.serviceType}</p>
+						<p>Details: {splitString(data.projectBrief, 'brief')}</p>
+					</div>
+				)}				
+      </div>
+    </div>,
+  );
 
-export interface ContactForm extends InferType<typeof contactFormSchema>{}
+export const contactFormSchema = object().shape(
+  {
+    addressLine1: string().when('delivery', {
+      is: deliveryOptions.inperson,
+      otherwise: (schema) => schema.nullable(),
+      then: (schema) => schema.required('Primary address is required'),
+    }),
+    addressLine2: string(),
+    budget: tuple([number().required(), number().required()]),
+    city: string().when('delivery', {
+      is: deliveryOptions.inperson,
+      otherwise: (schema) => schema.nullable(),
+      then: (schema) => schema.required('City is required'),
+    }),
+    country: string().when('delivery', {
+      is: deliveryOptions.inperson,
+      otherwise: (schema) => schema.nullable(),
+      then: (schema) => schema.required('Country is required'),
+    }),
+    delivery: string().when('serviceType', {
+			is: serviceTypes.cnc,
+			otherwise: (schema) => schema.nullable().notRequired(),
+			then: (schema) => schema.required('Delivery type is required'),
+		})
+		.when('serviceType', {
+			is: serviceTypes.design_fabrication,
+			otherwise: (schema) => schema.nullable().notRequired(),
+			then: (schema) => schema.required('Delivery type is required'),
+		}),
+    email: string().email().required('Email is required'),
+    firstName: string().required('First name is required'),
+    lastName: string().required('Last name is required'),
+    links: string()
+      .when('serviceType', {
+        is: serviceTypes.cnc,
+        otherwise: (schema) => schema.nullable().notRequired(),
+        then: (schema) => schema.required('Please provide links to your files'),
+      })
+      .when('serviceType', {
+        is: serviceTypes.design_fabrication,
+        otherwise: (schema) => schema.nullable().notRequired(),
+        then: (schema) => schema.required('Please provide links to your files'),
+      }),
+    phoneNumber: string().when('phoneNumber', {
+      is: (phoneNumber: string) => phoneNumber !== '',
+      otherwise: (schema) => schema.nullable(),
+      then: (schema) =>
+        schema.matches(phoneRegExp, 'Phone number is not valid'),
+    }),
+    projectBrief: string().required('Project brief is required'),
+    serviceType: string()
+      .oneOf(serviceTypesValues)
+      .required('Service type is required'),
+    state: string().when('delivery', {
+      is: deliveryOptions.inperson,
+      otherwise: (schema) => schema.notRequired(),
+      then: (schema) => schema.required('State is required'),
+    }),
+    timeline: tuple([number().required(), number().required()]),
+    zip: string().when('delivery', {
+      is: deliveryOptions.inperson,
+      otherwise: (schema) => schema.notRequired(),
+      then: (schema) => schema.required('Zip is required'),
+    }),
+  },
+  [
+    // Allow empty phone number.
+    ['phoneNumber', 'phoneNumber'],
+  ],
+);
+
+export interface ContactForm extends InferType<typeof contactFormSchema> {}
 
 export async function action({context, request}: ActionFunctionArgs) {
-	const SENDGRID_API_KEY = context.env.SENDGRID_API_KEY;
-	const SENDGRID_FROM_EMAIL = context.env.SENDGRID_FROM_EMAIL;
+  const SENDGRID_API_KEY = context.env.SENDGRID_API_KEY;
+  const SENDGRID_FROM_EMAIL = context.env.SENDGRID_FROM_EMAIL;
 
-	const formData = await request.formData();
-	let validData = contactForminitialValues as ContactForm;
-	for (const entry of formData.entries()) {
-		validData = {
-			...validData,
-			[entry[0]]: entry[1]
-		}
-	}
+  // Validate data.
+  const data = await request.json();
+  let validData = contactForminitialValues as ContactForm;
+  try {
+    validData = await contactFormSchema.validate(data, {
+      abortEarly: true,
+      stripUnknown: true,
+    });
+  } catch (e: ValidationError | any) {
+    if (e instanceof ValidationError) {
+      console.error('Error validating form', e.errors);
+      return json({error: e.errors, ok: false}, {status: 400});
+    }
+  }
 
-	// Validate data.
-	try {
-		validData = await contactFormSchema.validate(
-			validData,
-			{
-				abortEarly: true,
-				stripUnknown: true,
-			}
-		);
-	} catch(e: ValidationError | any) {
-		if (e instanceof ValidationError) {
-			return json({ error: e.errors, ok: false }, { status: 400 });
-		}
-	}
+  // Send email.
+  try {
+    if (!SENDGRID_API_KEY || !SENDGRID_FROM_EMAIL) {
+      throw new Error('Missing SendGrid API key or from email');
+    }
 
-	// Send email.
-	try {
-		// throw new Error('Error sending email');
+    const request = new Request('https://api.sendgrid.com/v3/mail/send');
+    const response = await fetch(request, {
+      body: JSON.stringify({
+        content: [
+          {
+            type: 'text/html',
+            value: submissionEmailFormat(validData),
+          },
+        ],
+        from: {
+          email: SENDGRID_FROM_EMAIL,
+          name: 'KLN Studios',
+        },
+        personalizations: [
+          {
+            to: [{email: SENDGRID_FROM_EMAIL}],
+          },
+        ],
+        reply_to: {email: SENDGRID_FROM_EMAIL},
+        subject: `Website Inquiry: ${validData.firstName} ${validData.lastName}, RE: ${validData.serviceType}`,
+      }),
+      headers: {
+        Authorization: `Bearer ${SENDGRID_API_KEY}`,
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    });
 
-		if (!SENDGRID_API_KEY || !SENDGRID_FROM_EMAIL) {
-			throw new Error('Missing SendGrid API key or from email');
-		}
-
-		const request = new Request( "https://api.sendgrid.com/v3/mail/send" );
-		const response = await fetch( request, {
-			body: JSON.stringify({
-				content: [
-					{
-						type: "text/plain",
-						value: submissionEmailFormat(validData),
-					},
-				],
-				from: {
-					email: SENDGRID_FROM_EMAIL,
-					name: "KLN Studios",
-				},
-				personalizations: [
-					{
-						to: [ { email: SENDGRID_FROM_EMAIL } ],
-					},
-				],
-				reply_to: { email: SENDGRID_FROM_EMAIL },
-				subject: `Website Inquiry: ${validData.firstName} ${validData.lastName}`,
-			}),
-			headers: {
-				Authorization: `Bearer ${SENDGRID_API_KEY}`,
-				"Content-Type": "application/json",
-			},
-			method: "POST",
-		});
-
-		if (response.ok) {
-			return json({ ok: true }, { status: 200 });
-		} else {
-			const r = await response.json();
-			console.error('Error sending email', JSON.stringify(r, null, 4));
-			throw new Error('Error sending email');
-		}
-	} catch(e) {
-		console.error('Error sending email', e);
-		return json({ error: ['403 Forbidden error'], ok: true }, { status: 403 });	
-	}
+    if (response.ok) {
+      return json({ok: true}, {status: 200});
+    } else {
+      const r = await response.json();
+      throw new Error('Error sending email');
+    }
+  } catch (e) {
+    return json({error: ['403 Forbidden error'], ok: true}, {status: 403});
+  }
 }
 
 export async function loader() {
-	throw new Response(null, {
-		status: 404,
-		statusText: 'Not Found',
-	});
+  throw new Response(null, {
+    status: 404,
+    statusText: 'Not Found',
+  });
 }
