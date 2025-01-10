1033a6da9e13462a33f289115d8293a62b509576
Joshua Ellis <joshua@wideeyecreative.com>
2025-01-10 11:56:20 -0500

    preview frameAncestors update

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -53,7 +53,7 @@ export const createCspHeaders = () => {
   const defaultsCSPHeaders = {
     connectSrc: ['*', "'self'"],
     fontSrc: ['*.sanity.io', "'self'", 'localhost:*', 'https://fonts.googleapis.com', 'https://fonts.gstatic.com'],
-    // frameAncestors: ['localhost:*', '*.sanity.studio'],
+    frameAncestors: ['localhost:*', '*.sanity.studio', '*.shopify.com', '*.shopifycdn.com', '*.myshopify.dev'],
     frameSrc: ["'self'"],
     imgSrc: ['*.sanity.io', 'https://cdn.shopify.com', "'self'", 'localhost:*', 'data:'],
     scriptSrc: ["'self'", 'localhost:*', 'https://cdn.shopify.com', 'https://fonts.googleapis.com', 'https://nyc.us22.list-manage.com'],
