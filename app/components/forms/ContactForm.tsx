ec90596eef34eaa4a2b7bce2e6768b56c9f75cf8
Joshua Ellis <joshua@wideeyecreative.com>
2025-01-14 16:35:32 -0500

    smooth out contact form animations

--- app/components/forms/ContactForm.tsx
+++ app/components/forms/ContactForm.tsx
@@ -1,4 +1,5 @@
 import type {ErrorMessageProps} from 'formik';
+import type {HTMLMotionProps} from 'framer-motion';
 
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import {AnimatePresence, m} from 'framer-motion';
@@ -13,6 +14,21 @@ import {SliderField} from './inputs/Range';
 export const labelClass = 'info-16 mb-2 block';
 export const inputRowClass = 'flex gap-2 flex-row mb-4';
 export const ErrorMessageWrapper = (props: ErrorMessageProps) => <ErrorMessage {...props} render={(msg) => <span className="sr-only error">{msg}</span>} />
+
+export const Collapsible = ({children, open = false, ...rest}: {
+	children: React.ReactNode,
+	open?: boolean,
+} & HTMLMotionProps<"div">) => {
+	return (
+		<AnimatePresence>
+			{open && (
+				<m.div {...rest}>
+					{children}
+				</m.div>
+			)}
+		</AnimatePresence>
+	)
+}
 
 /**
  * @todo
@@ -53,24 +69,24 @@ export function ContactForm() {
 						return (
 							<Form action='/contact' className='w-full mt-12 md:mt-20 mb-14 md:mb-24' ref={formRef}>
 								<AnimatePresence>
-								{props.status === 'success' ? (
-									<m.div
+									<Collapsible
 										animate={{ height: 'auto', opacity: 1 }}
 										className='overflow-y-hidden'
 										exit={{ height: 0, opacity: 0 }}
 										initial={{ height: 0, opacity: 0 }}
 										key="contact-thank-you"
+										open={props.status === 'success'}
 									>
 										<div className="min-h-52">
 											<h2 className="mb-6">Thank you for reaching out!</h2>
 											<p>We've received your information and will be in touch soon.</p>
 										</div>
-									</m.div>
-								) : (
-									<m.div
+									</Collapsible>
+									<Collapsible
 										exit={{ height: 0, opacity: 0 }}
 										initial={{ height: 'auto', opacity: 1 }}
 										key="contact-fields"
+										open={props.status !== 'success'}
 									>
 										<fieldset disabled={props.status === 'success'}>
 											<div className={cn(inputRowClass)}>
@@ -114,133 +130,130 @@ export function ContactForm() {
 											<div className={cn(inputRowClass)}>
 												<div className='grow'>
 													<label className={cn(labelClass)} htmlFor="projectBrief">Details
-														{props.values.serviceType === serviceTypes.cnc ? (
-															<m.span
-																animate={{ height: 'auto', opacity: 1 }}
-																className='block info-14 overflow-hidden'
-																exit={{ height: 0, opacity: 0 }}
-																initial={{ height: 0, opacity: 0 }}
-															>
-																Please provide a brief description of the project along with any supporting details, such as quantities, materials, material thicknesses, etc.
-															</m.span>
-														) : props.values.serviceType === serviceTypes.design_fabrication ? (
-															<m.span
-																animate={{ height: 'auto', opacity: 1 }}
-																className='block info-14 overflow-hidden'
-																exit={{ height: 0, opacity: 0 }}
-																initial={{ height: 0, opacity: 0 }}
-															>
-																Please provide a brief description of the project along with any supporting details, including where you are in the design process.
-															</m.span>
-														) : null}
+														<Collapsible
+															animate={{ height: 'auto', opacity: 1 }}
+															className='block info-14 overflow-hidden'
+															exit={{ height: 0, opacity: 0 }}
+															initial={{ height: 0, opacity: 0 }}
+															open={props.values.serviceType === serviceTypes.cnc}
+														>
+															Please provide a brief description of the project along with any supporting details, such as quantities, materials, material thicknesses, etc.
+														</Collapsible>
+														<Collapsible
+															animate={{ height: 'auto', opacity: 1 }}
+															className='block info-14 overflow-hidden'
+															exit={{ height: 0, opacity: 0 }}
+															initial={{ height: 0, opacity: 0 }}
+															open={props.values.serviceType === serviceTypes.design_fabrication}
+														>
+															Please provide a brief description of the project along with any supporting details, including where you are in the design process.
+														</Collapsible>
 													</label>
 													<Field as="textarea" className="default-input default-input--dark" name="projectBrief" required rows="4" type="text" />
 													<ErrorMessageWrapper name="projectBrief" />
 												</div>
 											</div>
-											{props.values.serviceType === serviceTypes.cnc || props.values.serviceType === serviceTypes.design_fabrication ? (
-												<m.div
+											<Collapsible
+												animate={{ height: 'auto', opacity: 1 }}
+												className='overflow-y-hidden'
+												exit={{ height: 0, opacity: 0 }}
+												initial={{ height: 0, opacity: 0 }}
+												key="contact-links"
+												open={props.values.serviceType === serviceTypes.cnc || props.values.serviceType === serviceTypes.design_fabrication}
+											>
+												<div className={cn(inputRowClass)}>
+													<div className='grow'>
+														<label className={cn(labelClass)} htmlFor="links">
+															Design File Links*
+															<br />
+															<span className="info-14"> Pease provide links to any current design files or reference images via Dropbox, GDrive, or another file hosting service.</span>
+														</label>
+														<Field as="textarea" className="default-input default-input--dark" name="links" required rows="4" type="text" />
+														<ErrorMessageWrapper name="links" />
+													</div>
+												</div>
+												<div className={cn(inputRowClass)} key="contact-timeline">
+													<div className='grow'>
+														<SliderField label="Timeline" max={6} min={1} name="timeline" postFix='Months' step={1} unlimited />
+													</div>
+												</div>
+												<div className={cn(inputRowClass)} key="contact-budget">
+													<div className='grow'>
+														<SliderField currency label="Budget" max={100000} min={5000} name="budget" step={5000} unlimited />
+													</div>
+												</div>
+												<div className={cn(inputRowClass)}>
+													<div className='grow'>
+														<label className={cn(labelClass)} htmlFor="delivery">Type of Service</label>
+														<Field as="select" className="default-input default-input--dark" name="delivery">
+															<option value="">Select a delivery option</option>
+															{deliveryOptionsValues.map((type) => (
+																<option key={type} value={type}>{type}</option>
+															))}
+														</Field>
+														<ErrorMessageWrapper name="delivery" />
+													</div>
+												</div>
+												<Collapsible
 													animate={{ height: 'auto', opacity: 1 }}
 													className='overflow-y-hidden'
 													exit={{ height: 0, opacity: 0 }}
 													initial={{ height: 0, opacity: 0 }}
-													key="contact-links"
+													key="contact-address"
+													open={props.values.delivery === deliveryOptions.inperson}
 												>
 													<div className={cn(inputRowClass)}>
 														<div className='grow'>
-															<label className={cn(labelClass)} htmlFor="links">
-																Design File Links*
-																<br />
-																<span className="info-14"> Pease provide links to any current design files or reference images via Dropbox, GDrive, or another file hosting service.</span>
-															</label>
-															<Field as="textarea" className="default-input default-input--dark" name="links" required rows="4" type="text" />
-															<ErrorMessageWrapper name="links" />
-														</div>
-													</div>
-													<div className={cn(inputRowClass)} key="contact-timeline">
-														<div className='grow'>
-															<SliderField label="Timeline" max={6} min={1} name="timeline" postFix='Months' step={1} unlimited />
-														</div>
-													</div>
-													<div className={cn(inputRowClass)} key="contact-budget">
-														<div className='grow'>
-															<SliderField currency label="Budget" max={100000} min={5000} name="budget" step={5000} unlimited />
+															<label className={cn(labelClass)} htmlFor="addressLine1">Address</label>
+															<Field className="default-input default-input--dark" name="addressLine1" required type="text" />
+															<ErrorMessageWrapper name="addressLine1" />
 														</div>
 													</div>
 													<div className={cn(inputRowClass)}>
 														<div className='grow'>
-															<label className={cn(labelClass)} htmlFor="delivery">Type of Service</label>
-															<Field as="select" className="default-input default-input--dark" name="delivery">
-																<option value="">Select a delivery option</option>
-																{deliveryOptionsValues.map((type) => (
-																	<option key={type} value={type}>{type}</option>
-																))}
-															</Field>
-															<ErrorMessageWrapper name="delivery" />
+															<label className={cn(labelClass)} htmlFor="addressLine2">Apt/Unit</label>
+															<Field className="default-input default-input--dark" name="addressLine2" type="text" />
+															<ErrorMessageWrapper name="addressLine2" />
 														</div>
 													</div>
-													{props.values.delivery === deliveryOptions.inperson ? (
-														<m.div
-															animate={{ height: 'auto', opacity: 1 }}
-															className='overflow-y-hidden'
-															exit={{ height: 0, opacity: 0 }}
-															initial={{ height: 0, opacity: 0 }}
-															key="contact-address"
-														>
-															<div className={cn(inputRowClass)}>
-																<div className='grow'>
-																	<label className={cn(labelClass)} htmlFor="addressLine1">Address</label>
-																	<Field className="default-input default-input--dark" name="addressLine1" required type="text" />
-																	<ErrorMessageWrapper name="addressLine1" />
-																</div>
-															</div>
-															<div className={cn(inputRowClass)}>
-																<div className='grow'>
-																	<label className={cn(labelClass)} htmlFor="addressLine2">Apt/Unit</label>
-																	<Field className="default-input default-input--dark" name="addressLine2" type="text" />
-																	<ErrorMessageWrapper name="addressLine2" />
-																</div>
-															</div>
-															<div className={cn(inputRowClass)}>
-																<div className='grow basis-1/2'>
-																	<label className={cn(labelClass)} htmlFor="city">City</label>
-																	<Field className="default-input default-input--dark" name="city" required type="text" />
-																	<ErrorMessageWrapper name="city" />
-																</div>
-																<div className='grow basis-1/2'>
-																	<label className={cn(labelClass)} htmlFor="state">State</label>
-																	<Field className="default-input default-input--dark" name="state" required type="text" />
-																	<ErrorMessageWrapper name="state" />
-																</div>
-															</div>
-															<div className={cn(inputRowClass)}>
-																<div className='grow basis-1/2'>
-																	<label className={cn(labelClass)} htmlFor="zip">Zip</label>
-																	<Field className="default-input default-input--dark" name="zip" required type="text" />
-																	<ErrorMessageWrapper name="zip" />
-																</div>
-																<div className='grow basis-1/2'>
-																	<label className={cn(labelClass)} htmlFor="country">Country</label>
-																	<Field className="default-input default-input--dark" name="country" required type="text" />
-																	<ErrorMessageWrapper name="country" />
-																</div>
-															</div>
-														</m.div>
-													) : props.values.delivery === deliveryOptions.pickup ? (
-														<m.div
-															animate={{ height: 'auto', opacity: 1 }}
-															className='overflow-y-hidden'
-															exit={{ height: 0, opacity: 0 }}
-															initial={{ height: 0, opacity: 0 }}
-															key="contact-pickup"
-														>
-															<div className={cn(inputRowClass)}>
-																<p className="info-16">Pick up occurs from our East Williamsburg shop location @ 79 Grattan St, Brooklyn, NY 11237.</p>
-															</div>
-														</m.div>
-													) : null}
-												</m.div>
-											) : null}
+													<div className={cn(inputRowClass)}>
+														<div className='grow basis-1/2'>
+															<label className={cn(labelClass)} htmlFor="city">City</label>
+															<Field className="default-input default-input--dark" name="city" required type="text" />
+															<ErrorMessageWrapper name="city" />
+														</div>
+														<div className='grow basis-1/2'>
+															<label className={cn(labelClass)} htmlFor="state">State</label>
+															<Field className="default-input default-input--dark" name="state" required type="text" />
+															<ErrorMessageWrapper name="state" />
+														</div>
+													</div>
+													<div className={cn(inputRowClass)}>
+														<div className='grow basis-1/2'>
+															<label className={cn(labelClass)} htmlFor="zip">Zip</label>
+															<Field className="default-input default-input--dark" name="zip" required type="text" />
+															<ErrorMessageWrapper name="zip" />
+														</div>
+														<div className='grow basis-1/2'>
+															<label className={cn(labelClass)} htmlFor="country">Country</label>
+															<Field className="default-input default-input--dark" name="country" required type="text" />
+															<ErrorMessageWrapper name="country" />
+														</div>
+													</div>
+												</Collapsible>
+												<Collapsible
+													animate={{ height: 'auto', opacity: 1 }}
+													className='overflow-y-hidden'
+													exit={{ height: 0, opacity: 0 }}
+													initial={{ height: 0, opacity: 0 }}
+													key="contact-pickup"
+													open={props.values.delivery === deliveryOptions.pickup}
+												>
+													<div className={cn(inputRowClass)}>
+														<p className="info-16">Pick up occurs from our East Williamsburg shop location @ 79 Grattan St, Brooklyn, NY 11237.</p>
+													</div>
+												</Collapsible>
+											</Collapsible>
 											<div className={cn(inputRowClass)}>
 												<Button className="w-full h-[48px] md:h-[60px]" onClick={(e) => {
 													e.preventDefault();
@@ -263,8 +276,7 @@ export function ContactForm() {
 												</div>
 											</div>
 										</fieldset>
-									</m.div>
-								)}
+									</Collapsible>
 								</AnimatePresence>
 							</Form>
 						)	
